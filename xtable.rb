309e3c7dad96f3e58d5681e42d40951a6914d03b
johnram528 <johnram528@gmail.com>
2017-09-16 01:17:19 -0400

    method completed

--- xtable.rb
+++ xtable.rb
@@ -1,3 +1,10 @@
 def timesTable(int)
-
+  initial_array = (1..int).to_a #grab range from 1 to integer and turn into array
+  arrays = []
+  initial_array.each do |num|
+    array = initial_array.collect {|n| n * num} #take each int in initial array and creat new array multiplied by int
+    arrays << array.join(', ') #turn arrays into string before pusing 
+  end
+  arrays.each {|arr| puts arr.gsub(/[\s,]/ ," ")} 
 end
+
